91b074b0238faba3a40ded836cfe575d97076211
albertbyrone <albertbyrone16777@gmail.com>
2020-12-15 17:16:48 +0300

    add the loader

---
+++ src/components/loader/loader.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+
+// import { RED } from "../../utils/colors";
+
+const Spinner = styled(CircularProgress )`
+  && {
+    color: blue;
+  }
+`;
+
+const SpinnerWrapper = styled.div`
+  align-self: center;
+`;
+
+const Loader = () => (
+  <SpinnerWrapper>
+    <Spinner />
+  </SpinnerWrapper>
+);
+
+export default Loader;
