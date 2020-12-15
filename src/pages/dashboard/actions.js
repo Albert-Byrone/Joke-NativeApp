5104d87915056422ebac187bb7c92766a6a1b580
albertbyrone <albertbyrone16777@gmail.com>
2020-12-15 17:23:33 +0300

    a function thast is calles when data is loaded suuceesfuly

--- src/pages/dashboard/actions.js
+++ src/pages/dashboard/actions.js
@@ -1,3 +1,8 @@
+const loadCategoriesSucceeded = ({ categories }) => ({
+  type: LOAD_CATEGORIES_SUCCEEDED,
+  categories
+});
+
 const loadCategoriesFailed = ({ error }) => ({
   type: LOAD_CATEGORIES_FAILED,
   error
